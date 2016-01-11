b69bf4018713bfff99a6391d48a2f91a01b670f1
abrahamsk <abrahamskatie@gmail.com>
2016-01-10 16:17:08 -0800

    Letter data properly scaled now

--- letter.py
+++ letter.py
@@ -5,11 +5,17 @@
 # Katie Abrahams, abrahake@pdx.edu
 # 1/19/16
 
+import numpy as np
+
+max_letter_attribute_val = 15
+
 class letter:
     """Letter entity class"""
     value = None # alphabet letter
-    attributes = []  # 16 numerical attributes for a letter from the dataset
+    attributes = []  # 16 numerical attributes for a letter from the data set
 
     def __init__(self, input):
         self.value = input[:1]
-        self.attributes = map(int, input[2:])
+        self.attributes = map(float, input[2:])
+        # scale each data value to be between 0 and 1 by dividing by 15 (max val = 15)
+        self.attributes = [i/max_letter_attribute_val for i in self.attributes]
