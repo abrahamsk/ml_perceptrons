7eaec14e62d67414a824953deb3a9232a4cbbe13
abrahamsk <abrahamskatie@gmail.com>
2016-01-10 16:03:13 -0800

    Comments in input.py cleaned up, training data processed to letter classes

--- input.py
+++ input.py
@@ -21,44 +21,14 @@ testing_data = data[10000:] # data from data[10000] to the end of the list
 training_data = sorted(training_data)
 testing_data = sorted(training_data)
 
-# with open('training_data_test.txt', 'w+') as f_training_out:
-#      for row in training_data:
-#          ###print row
-#          f_training_out.write("%s\n" % str(row))
-#
-# with open('testing_data_test.txt', 'w+') as f_testing_out:
-#      for row in testing_data:
-#          ###print row
-#          f_testing_out.write("%s\n" % str(row))
-
-letters_list = []
+# list of letters from training data
+letters_list_training = []
 for (i, training_data) in enumerate(training_data):
-    letters_list.append(letter(training_data.split(',')))
-    print letters_list[i].value
-    print letters_list[i].attributes
+    letters_list_training.append(letter(training_data.split(',')))
+    print letters_list_training[i].value
+    print letters_list_training[i].attributes
 
-# letters_list = []
-#
-# for i in enumerate(training_data):
-#     letters_list.append(letter(training_data.split(',')))
-#
-# #later
-#
-# for obj in letters_list:
-#     print letters_list.value
-
-
-# # instantiate letter and perceptron class
-# for (i, training_data) in enumerate(training_data):
-#     ###print i, training_data[:1], training_data[1:], '\n', training_data
-#     ###training_data = training_data.replace(',','')
-#     a_training_letter = letter(training_data.split(','))
 # ###perceptron = perceptron('blarg')
-
-# read in from files
-### with open('letter-recognition_training.data') as f:
-###     lines = (line.rstrip('\n') for line in open(f))
-
 # perceptron inputs are letter attributes from the training set attributes
 # 16 attributes from letter -> perceptron inputs
 ###perceptron_inputs = a_training_letter.attributes
@@ -66,11 +36,3 @@ for (i, training_data) in enumerate(training_data):
 # print line debugging!
 ###print perceptron.weights
 ###print perceptron.bias
-# print a_training_letter.value
-# print a_training_letter.attributes
-# print a_training_letter.attributes[0]
-# print a_training_letter.attributes[1]
-# print a_training_letter.attributes[2]
-# print a_training_letter.attributes[3]
-# total = a_training_letter.attributes[3]+a_training_letter.attributes[3]
-# print total
