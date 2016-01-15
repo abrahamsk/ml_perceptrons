8f93bf7be5fd22fabdca387c21a47cdb1a6120bb
abrahamsk <abrahamskatie@gmail.com>
2016-01-14 19:45:57 -0800

    Test confusion matrix #2

--- testing.py
+++ testing.py
@@ -11,8 +11,15 @@ from input import letters_list_testing
 import sklearn
 from sklearn.metrics import confusion_matrix
 import pandas
+import pandas as pd
+from pandas_confusion import ConfusionMatrix
 
 y_actual = [2, 0, 2, 2, 0, 1, 1, 2, 2, 0, 1, 2]
 x_predicted = [0, 0, 2, 1, 0, 2, 1, 0, 2, 0, 2, 2]
-confusion_matrix(x_predicted, y_actual)
-print confusion_matrix(x_predicted, y_actual)
+# confusion_matrix(x_predicted, y_actual)
+# print confusion_matrix(x_predicted, y_actual)
+
+
+cm = ConfusionMatrix(y_actual, x_predicted)
+cm.print_stats()
+
